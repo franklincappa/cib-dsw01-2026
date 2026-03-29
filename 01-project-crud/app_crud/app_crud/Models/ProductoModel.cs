d1e8a051ce983d73f8f3c6999418cc9768e88f55
Franklin Cappa Ticona <franklincappa@gmail.com>
2026-03-28 19:35:40 -0500

    Proyecto CRUD

---
+++ 01-project-crud/app_crud/app_crud/Models/ProductoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace app_crud.Models
+{
+    public class ProductoModel
+    {
+        [Display(Name = "Id Producto")]
+        public int idProducto { get; set; }
+        [Display(Name = "Descripción")]
+        public string descripcion { get; set; }
+        [Display(Name = "Und Medida")]
+        public string uMedida { get; set; }
+        [Display(Name = "Precio Unitario")]
+        public decimal precio { get; set; }
+        [Display(Name = "Stock")]
+        public int stock { get; set; }
+    }
+}
