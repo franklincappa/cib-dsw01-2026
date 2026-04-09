d014cdbf99c5855e7f367acfba5a39d7cd5471a6
Franklin Cappa Ticona <franklincappa@gmail.com>
2026-04-08 21:22:59 -0500

    WebApi Cibertec

---
+++ 02-project-webApi/app-productoAPI/app-productoAPI/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using app_productoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_productoAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>( entity =>
+            {
+                entity.ToTable("Producto");
+                entity.HasKey(e => e.IdProducto);
+                entity.Property(e => e.IdProducto).HasColumnName("idProducto");
+                entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+                entity.Property(e => e.UMedida).HasColumnName("uMedida");
+                entity.Property(e => e.Precio).HasColumnName("precio");
+                entity.Property(e => e.Stock).HasColumnName("stock");
+
+            });
+        }
+    }
+}
---
+++ 02-project-webApi/app-productoAPI/app-productoAPI/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_productoAPI.Models
+{
+    [Table("producto")]
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdProducto { get; set; }
+        [StringLength(50)]
+        public string? Descripcion { get; set; }
+        [StringLength(50)]
+        public string? UMedida { get; set; }
+        [Column(TypeName = "decimal(7,2)")]
+        public decimal? Precio { get; set; }
+        public int? Stock { get; set; }
+    }
+}
